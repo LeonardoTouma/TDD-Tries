d55733881155c521521981ddb1f9f2e97ca3100f
Leonardo Touma <toumaleonardo@gmail.com>
2017-05-23 17:52:32 +0200

    Skapat 2st class libraries, ena är test andra är BI, tester fungerar inte av någon anledning

---
+++ TDD-TRIES/TDD-TESTS/InfoNeeded.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_TESTS
+{
+    public class InfoNeeded
+    {
+            public InfoNeeded()
+            {
+            }
+
+            public void GetInfo(string hej)
+            {
+                string hej1 = "";
+                hej1 += hej;
+            }
+        }
+}
---
+++ TDD-TRIES/TDD-TESTS/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_TESTS
+{
+    [TestFixture]
+    public class Tests
+    {
+            [Test]
+            public void CheckInfo(string info)
+            {
+                var infos = new InfoNeeded();
+                string hej = "tjena";
+                infos.GetInfo(hej);
+                Assert.AreEqual("tjena", hej, "Value of hej:");
+            }
+     }
+}
